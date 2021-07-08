aa87d4db8da9b3d0e3e6281a95bca4fb02152183
maximshen007 <maximszc@gmail.com>
2021-07-08 13:12:26 -0400

    add test script for issue 60

--- exchange_api/injective_exch_issue_60.py
+++ exchange_api/injective_exch_issue_60.py
@@ -36,6 +36,7 @@ async def main() -> None:
     async with grpc.aio.insecure_channel('localhost:9910') as channel:
         exchange_rpc = exchange_rpc_grpc.InjectiveExchangeRPCStub(channel)
         spot_exchange_rpc = spot_exchange_rpc_grpc.InjectiveSpotExchangeRPCStub(channel)
+        
 
         resp = await exchange_rpc.Version(exchange_rpc_pb.VersionRequest())
         print("-- Connected to Injective Exchange (version: %s, built %s)" % (resp.version, resp.meta_data["BuildDate"]))
@@ -46,8 +47,16 @@ async def main() -> None:
             print(m.ticker, "=", m.market_id)
 
         selected_market = resp.markets[0]
+
+        mkt_id = "0x17d9b5fb67666df72a5a858eb9b81104b99da760e3036a8243e05532d50e1c7c"
+        acct_id = "0xbdaedec95d563fb05240d6e01821008454c24c36000000000000000000000000"
+        
         print("\n-- Watching for order updates on market %s" % selected_market.ticker)
-        stream_req = spot_exchange_rpc_pb.StreamOrdersRequest(market_id=selected_market.market_id)
+        #success ->stream_req = spot_exchange_rpc_pb.StreamOrdersRequest(market_id=selected_market.market_id)
+        #success -> stream_req = spot_exchange_rpc_pb.StreamOrdersRequest(market_id=selected_market.market_id, order_type='buy')
+        
+        # this fails 
+        stream_req = spot_exchange_rpc_pb.StreamOrdersRequest(market_id=selected_market.market_id, subaccount_id=acct_id)
         orders_stream = spot_exchange_rpc.StreamOrders(stream_req)
         async for order in orders_stream:
             print("\n-- Order Update:", order)
@@ -56,3 +65,12 @@ async def main() -> None:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     asyncio.get_event_loop().run_until_complete(main())
+
+
+
+
+
+
+
+
+
