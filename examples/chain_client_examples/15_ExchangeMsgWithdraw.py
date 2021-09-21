8d1e82340b470346f97ccd7c7fe961d5ca36f2b6
Achilleas <kalantzis.achilleas@gmail.com>
2021-09-20 20:34:13 -0400

    add examples

---
+++ examples/chain_client_examples/15_ExchangeMsgWithdraw.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+
+from pyinjective.composer import Composer as ProtoMsgComposer
+from pyinjective.client import Client
+from pyinjective.transaction import Transaction
+from pyinjective.constant import Network
+from pyinjective.wallet import PrivateKey, PublicKey, Address
+
+async def main() -> None:
+    # select network: local, testnet, mainnet
+    network = Network.testnet()
+
+    # initialize grpc client
+    client = Client(network.grpc_endpoint, insecure=True)
+
+    # load account
+    priv_key = PrivateKey.from_hex("f9db9bf330e23cb7839039e944adef6e9df447b90b503d5b4464c90bea9022f3")
+    pub_key =  priv_key.to_public_key()
+    address = pub_key.to_address()
+    subaccount_id = address.get_subaccount_id(index=0)
+
+    # prepare trade info
+    market_id = "0x0f4209dbe160ce7b09559c69012d2f5fd73070f8552699a9b77aebda16ccdeb1"
+
+    # prepare tx msg
+    msg = ProtoMsgComposer.MsgWithdraw(
+        sender=address.to_acc_bech32(),
+        subaccount_id=subaccount_id,
+        amount=1000000000000000000,
+        denom="inj"
+    )
+
+    acc_num, acc_seq = await address.get_num_seq(network.lcd_endpoint)
+    gas_price = 500000000
+    gas_limit = 200000
+    fee = [ProtoMsgComposer.Coin(
+        amount=str(gas_price * gas_limit),
+        denom=network.fee_denom,
+    )]
+
+    # build tx
+    tx = (
+        Transaction()
+        .with_messages(msg)
+        .with_sequence(acc_seq)
+        .with_account_num(acc_num)
+        .with_chain_id(network.chain_id)
+        .with_gas(gas_limit)
+        .with_fee(fee)
+        .with_memo("")
+        .with_timeout_height(0)
+    )
+
+    # build signed tx
+    sign_doc = tx.get_sign_doc(pub_key)
+    sig = priv_key.sign(sign_doc.SerializeToString())
+    tx_raw_bytes = tx.get_tx_data(sig, pub_key)
+
+    # broadcast tx: send_tx_async_mode, send_tx_sync_mode, send_tx_block_mode
+    res = client.send_tx_block_mode(tx_raw_bytes)
+
+    # print tx response
+    print(res)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(main())
