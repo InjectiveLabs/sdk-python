5a2555d64a7794c0db540716377ee64300b8ed56
Achilleas <kalantzis.achilleas@gmail.com>
2021-12-03 06:52:42 -0500

    add auction rpc

---
+++ examples/exchange_api_examples/auctions_rpc/9_Streambids.py
@@ -0,0 +1,31 @@
+# Copyright 2021 Injective Labs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Injective Exchange API client for Python. Example only."""
+
+import asyncio
+import logging
+
+from pyinjective.client import Client
+from pyinjective.constant import Network
+
+async def main() -> None:
+    network = Network.testnet()
+    client = Client(network, insecure=True)
+    bids = client.stream_bids()
+    for bid in bids:
+        print(bid)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(main())
