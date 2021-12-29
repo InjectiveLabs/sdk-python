58a22b6fce3d8bd78a1ee485433438515ad3cf05
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2021-12-29 18:10:21 +0200

    Add MsgBatchUpdateOrders example
    Add MsgBatchUpdateOrders example

---
+++ examples/sync/chain_client/19_ExchangeMsgBatchUpdateOrders.py
@@ -0,0 +1,177 @@
+# Copyright 2021 Injective Labs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Injective Chain Tx/Query client for Python. Example only."""
+
+import asyncio
+import logging
+
+from pyinjective.composer import Composer as ProtoMsgComposer
+from pyinjective.client import Client
+from pyinjective.transaction import Transaction
+from pyinjective.constant import Network
+from pyinjective.wallet import PrivateKey, PublicKey, Address
+
+
+async def main() -> None:
+    # select network: local, testnet, mainnet
+    network = Network.testnet()
+    composer = ProtoMsgComposer(network=network.string())
+
+    # initialize grpc client
+    client = Client(network, insecure=True)
+
+    # load account
+    priv_key = PrivateKey.from_hex("5d386fbdbf11f1141010f81a46b40f94887367562bd33b452bbaa6ce1cd1381e")
+    pub_key = priv_key.to_public_key()
+    address = pub_key.to_address().init_num_seq(network.lcd_endpoint)
+    subaccount_id = address.get_subaccount_id(index=0)
+
+    # prepare trade info
+    fee_recipient = "inj1hkhdaj2a2clmq5jq6mspsggqs32vynpk228q3r"
+
+    derivative_market_id_create = "0x7cc8b10d7deb61e744ef83bdec2bbcf4a056867e89b062c6a453020ca82bd4e4"
+    spot_market_id_create = "0xa508cb32923323679f29a032c70342c147c17d0145625922b0ef22e955c844c0"
+
+    derivative_market_id_cancel = "0x1f73e21972972c69c03fb105a5864592ac2b47996ffea3c500d1ea2d20138717"
+    derivative_market_id_cancel_2 = "0x8158e603fb80c4e417696b0e98765b4ca89dcf886d3b9b2b90dc15bfb1aebd51"
+    spot_market_id_cancel = "0x74b17b0d6855feba39f1f7ab1e8bad0363bd510ee1dcc74e40c2adfe1502f781"
+    spot_market_id_cancel_2 = "0x01edfab47f124748dc89998eb33144af734484ba07099014594321729a0ca16b"
+
+    spot_market_ids_to_cancel_all =['0x28f3c9897e23750bf653889224f93390c467b83c86d736af79431958fff833d1', '0xe8bf0467208c24209c1cf0fd64833fa43eb6e8035869f9d043dbff815ab76d01']
+    derivative_market_ids_to_cancel_all = ['0x4ca0f92fc28be0c9761326016b5a1a2177dd6375558365116b5bdda9abc229ce', '0x979731deaaf17d26b2e256ad18fecd0ac742b3746b9ea5382bac9bd0b5e58f74']
+
+    derivative_orders_to_cancel = [
+        composer.OrderData(
+            market_id=derivative_market_id_cancel,
+            subaccount_id=subaccount_id,
+            order_hash="0xd6edebb7ea4ce617c2ab30b42c8793260d4d28e1966403b5aca986d7c0349be1"
+        ),
+        composer.OrderData(
+            market_id=derivative_market_id_cancel_2,
+            subaccount_id=subaccount_id,
+            order_hash="0x7ee76255d7ca763c56b0eab9828fca89fdd3739645501c8a80f58b62b4f76da5"
+        )
+    ]
+
+    spot_orders_to_cancel = [
+        composer.OrderData(
+            market_id=spot_market_id_cancel,
+            subaccount_id=subaccount_id,
+            order_hash="0x3870fbdd91f07d54425147b1bb96404f4f043ba6335b422a6d494d285b387f2d"
+        ),
+        composer.OrderData(
+            market_id=spot_market_id_cancel_2,
+            subaccount_id=subaccount_id,
+            order_hash="0x222daa22f60fe9f075ed0ca583459e121c23e64431c3fbffdedda04598ede0d2"
+        )
+    ]
+
+    derivative_orders_to_create = [
+        composer.DerivativeOrder(
+            market_id=derivative_market_id_create,
+            subaccount_id=subaccount_id,
+            fee_recipient=fee_recipient,
+            price=3,
+            quantity=55,
+            leverage=1,
+            is_buy=True
+        ),
+        composer.DerivativeOrder(
+            market_id=derivative_market_id_create,
+            subaccount_id=subaccount_id,
+            fee_recipient=fee_recipient,
+            price=300,
+            quantity=55,
+            leverage=1,
+            is_buy=False,
+        ),
+    ]
+
+    spot_orders_to_create = [
+        composer.SpotOrder(
+            market_id=spot_market_id_create,
+            subaccount_id=subaccount_id,
+            fee_recipient=fee_recipient,
+            price=3,
+            quantity=55,
+            is_buy=True
+        ),
+        composer.SpotOrder(
+            market_id=spot_market_id_create,
+            subaccount_id=subaccount_id,
+            fee_recipient=fee_recipient,
+            price=300,
+            quantity=55,
+            is_buy=False
+        ),
+    ]
+
+    # prepare tx msg
+    msg = composer.MsgBatchUpdateOrders(
+        sender=address.to_acc_bech32(),
+        subaccount_id=subaccount_id,
+        derivative_orders_to_create=derivative_orders_to_create,
+        spot_orders_to_create=spot_orders_to_create,
+        derivative_orders_to_cancel=derivative_orders_to_cancel,
+        spot_orders_to_cancel=spot_orders_to_cancel,
+        spot_market_ids_to_cancel_all=spot_market_ids_to_cancel_all,
+        derivative_market_ids_to_cancel_all=derivative_market_ids_to_cancel_all,
+    )
+
+    # build sim tx
+    tx = (
+        Transaction()
+        .with_messages(msg)
+        .with_sequence(address.get_sequence())
+        .with_account_num(address.get_number())
+        .with_chain_id(network.chain_id)
+    )
+    sim_sign_doc = tx.get_sign_doc(pub_key)
+    sim_sig = priv_key.sign(sim_sign_doc.SerializeToString())
+    sim_tx_raw_bytes = tx.get_tx_data(sim_sig, pub_key)
+
+    # simulate tx
+    (sim_res, success) = client.simulate_tx(sim_tx_raw_bytes)
+    if not success:
+        print(sim_res)
+        return
+
+    sim_res_msg = ProtoMsgComposer.MsgResponses(sim_res.result.data, simulation=True)
+    print("simulation msg response")
+    print(sim_res_msg)
+
+    # build tx
+    gas_price = 500000000
+    gas_limit = sim_res.gas_info.gas_used + 15000 # add 15k for gas, fee computation
+    fee = [composer.Coin(
+        amount=gas_price * gas_limit,
+        denom=network.fee_denom,
+    )]
+    current_height = client.get_latest_block().block.header.height
+    tx = tx.with_gas(gas_limit).with_fee(fee).with_memo("").with_timeout_height(current_height+50)
+    sign_doc = tx.get_sign_doc(pub_key)
+    sig = priv_key.sign(sign_doc.SerializeToString())
+    tx_raw_bytes = tx.get_tx_data(sig, pub_key)
+
+    # broadcast tx: send_tx_async_mode, send_tx_sync_mode, send_tx_block_mode
+    res = client.send_tx_block_mode(tx_raw_bytes)
+    res_msg = ProtoMsgComposer.MsgResponses(res.data)
+    print("tx response")
+    print(res)
+    print("tx msg response")
+    print(res_msg)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(main())
