0a8d2146fd2d74d98f932a7ff19a880ef0cc3907
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2021-12-29 15:53:37 +0200

    Add skip/limit to funding payments
    Add skip/limit to funding payments

--- pyinjective/async_client.py
+++ pyinjective/async_client.py
@@ -364,5 +364,5 @@ class AsyncClient:
         return await self.stubDerivativeExchange.SubaccountTradesList(req)
 
     async def get_funding_payments(self, subaccount_id: str, **kwargs):
-        req = derivative_exchange_rpc_pb.FundingPaymentsRequest(subaccount_id=subaccount_id, market_id=kwargs.get("market_id"))
+        req = derivative_exchange_rpc_pb.FundingPaymentsRequest(subaccount_id=subaccount_id, market_id=kwargs.get("market_id"), skip=kwargs.get("skip"), limit=kwargs.get("limit"))
         return await self.stubDerivativeExchange.FundingPayments(req)
