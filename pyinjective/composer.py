5a5d0983f30ab5fbe9d9d8760271066211325c24
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-01-18 13:21:04 +0200

    fix: modify the authz MsgResponses

--- pyinjective/composer.py
+++ pyinjective/composer.py
@@ -433,9 +433,9 @@ class Composer:
             "/injective.exchange.v1beta1.MsgIncreasePositionMargin": injective_exchange_tx_pb.MsgIncreasePositionMarginResponse,
             "/injective.auction.v1beta1.MsgBid": injective_auction_tx_pb.MsgBidResponse,
             "/cosmos.bank.v1beta1.MsgSend": cosmos_bank_tx_pb.MsgSendResponse,
-            "/cosmos.authz.v1beta1.MsgGrant": cosmos_authz_tx_pb.MsgGrant,
-            "/cosmos.authz.v1beta1.MsgExec": cosmos_authz_tx_pb.MsgExec,
-            "/cosmos.authz.v1beta1.MsgRevoke": cosmos_authz_tx_pb.MsgRevoke,
+            "/cosmos.authz.v1beta1.MsgGrant": cosmos_authz_tx_pb.MsgGrantResponse,
+            "/cosmos.authz.v1beta1.MsgExec": cosmos_authz_tx_pb.MsgExecResponse,
+            "/cosmos.authz.v1beta1.MsgRevoke": cosmos_authz_tx_pb.MsgRevokeResponse,
         }
 
         response = tx_response_pb.TxResponseData.FromString(data)
