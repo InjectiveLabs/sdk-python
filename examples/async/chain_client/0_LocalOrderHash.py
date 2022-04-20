e386a60a2b83845595f75a8a90e870fd1878bd3e
nhannamsiu <dangduynam95@gmail.com>
2022-04-20 21:35:19 +0700

    remove local import path

--- examples/async/chain_client/0_LocalOrderHash.py
+++ examples/async/chain_client/0_LocalOrderHash.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Injective Chain Tx/Query client for Python. Example only."""
-import sys
-sys.path.insert(0, '/Users/nam/desktop/injective/sdk-python/')
 
 import asyncio
 import logging
