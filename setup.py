b0fd330fcdd7933d0548d27b19538e646b6d5ac6
Achilleas Kalantzis <kalantzis.achilleas@gmail.com>
2022-09-15 22:57:19 +0300

    chore: release 0.5.8.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = "https://github.com/InjectiveLabs/sdk-python"
 EMAIL = "achilleas@injectivelabs.com"
 AUTHOR = "Injective Labs"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.5.8.1"
+VERSION = "0.5.8.2"
 
 REQUIRED = [
     "grpcio",
