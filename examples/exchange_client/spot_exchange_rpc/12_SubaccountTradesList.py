72c93239e758d6ff0923e787c880fe1897bfef08
hanssun123 <hanssun123@users.noreply.github.com>
2023-02-11 01:41:09 -0400

    Update SpotExchangeRPC Examples

--- examples/exchange_client/spot_exchange_rpc/12_SubaccountTradesList.py
+++ examples/exchange_client/spot_exchange_rpc/12_SubaccountTradesList.py
@@ -5,14 +5,15 @@ from pyinjective.async_client import AsyncClient
 from pyinjective.constant import Network
 
 async def main() -> None:
+    # select network: local, testnet, mainnet
     network = Network.testnet()
     client = AsyncClient(network, insecure=False)
     subaccount_id = "0xaf79152ac5df276d9a8e1e2e22822f9713474902000000000000000000000000"
     market_id = "0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe"
     execution_type = "market"
     direction = "buy"
-    skip = 10
-    limit = 10
+    skip = 2
+    limit = 3
     trades = await client.get_spot_subaccount_trades(
         subaccount_id=subaccount_id,
         market_id=market_id,
