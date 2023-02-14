601a52e682ae414abf7e203be01c5ec4c2e5805f
hanssun123 <hanssun123@users.noreply.github.com>
2023-02-14 00:04:06 -0400

    Create 1_accountPortfolio.py

---
+++ examples/exchange_client/portfolio_rpc/1_accountPortfolio.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+
+from pyinjective.async_client import AsyncClient
+from pyinjective.constant import Network
+
+async def main() -> None:
+    # select network: local, testnet, mainnet
+    network = Network.testnet()
+    client = AsyncClient(network, insecure=False)
+    account_address = "inj1clw20s2uxeyxtam6f7m84vgae92s9eh7vygagt"
+    portfolio = await client.get_account_portfolio(
+        account_address=account_address
+    )
+    print(portfolio)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(main())
