2aa39fa805c43b37540758daeda88b6599850443
hanssun123 <hanssun123@users.noreply.github.com>
2023-02-17 02:48:53 -0400

    Update async_client.py
    Add method for orderbooksV2

--- pyinjective/async_client.py
+++ pyinjective/async_client.py
@@ -640,6 +640,10 @@ class AsyncClient:
         req = spot_exchange_rpc_pb.OrderbooksRequest(market_ids=market_ids)
         return await self.stubSpotExchange.Orderbooks(req)
 
+    async def get_spot_orderbooksV2(self, market_ids: List):
+        req = spot_exchange_rpc_pb.OrderbooksV2Request(market_ids=market_ids)
+        return await self.stubSpotExchange.OrderbooksV2(req)
+
     async def get_spot_orders(self, market_id: str, **kwargs):
         req = spot_exchange_rpc_pb.OrdersRequest(
             market_id=market_id,
