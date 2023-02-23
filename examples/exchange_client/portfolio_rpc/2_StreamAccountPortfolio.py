a403d6ef84d6fd8ef2b749cf8a3a27c4aa6fbd01
Hilari Moragrega <hilari@hilarimoragrega.com>
2023-02-23 17:46:59 +0700

    feat(exchange): add example for streaming account portfolio API

---
+++ examples/exchange_client/portfolio_rpc/2_StreamAccountPortfolio.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging
+
+from pyinjective.async_client import AsyncClient
+from pyinjective.constant import Network
+
+
+async def main() -> None:
+    network = Network.testnet()
+    client = AsyncClient(network, insecure=False)
+    account_address = "inj1clw20s2uxeyxtam6f7m84vgae92s9eh7vygagt"
+    updates = await client.stream_account_portfolio(account_address=account_address)
+    async for update in updates:
+        print("Account portfolio Update:\n")
+        print(update)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.get_event_loop().run_until_complete(main())
