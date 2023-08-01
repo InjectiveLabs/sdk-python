1e71d012ced9d197df1fe0ffab64170ba3e79f9e
abel <abarmoa@gmail.com>
2023-08-01 13:27:34 -0300

    (fix) Update version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = "https://github.com/InjectiveLabs/sdk-python"
 EMAIL = "achilleas@injectivelabs.com"
 AUTHOR = "Injective Labs"
 REQUIRES_PYTHON = ">=3.9"
-VERSION = "0.7.0.3"
+VERSION = "0.7.0.4"
 
 REQUIRED = [
     "aiohttp",
