364d78c3e695f186dfc9c0d936fd1e95843ba564
abel <abarmoa@gmail.com>
2023-09-07 11:25:55 -0300

    (feat) Finished configuraton of chain stream example module

--- examples/chain_client/49_ChainStream.py
+++ examples/chain_client/49_ChainStream.py
@@ -8,35 +8,51 @@ from pyinjective.core.network import Network
 
 
 async def main() -> None:
-    # select network: local, testnet, mainnet
-    # network = Network.devnet()
-    network = Network.custom(
-        lcd_endpoint="https://staging.lcd.injective.network:443",
-        tm_websocket_endpoint="wss://staging.tm.injective.network:443/websocket",
-        grpc_endpoint="staging.chain.grpc.injective.network:443",
-        grpc_exchange_endpoint="staging.exchange.grpc.injective.network:443",
-        grpc_explorer_endpoint="staging.explorer.grpc.injective.network:443",
-        chain_stream_endpoint="staging.stream.injective.network:443",
-        chain_id="injective-1",
-        env='mainnet',
-        use_secure_connection=True,
-    )
+    network = Network.devnet()
 
     client = AsyncClient(network)
     composer = Composer(network=network.string())
 
-    inj_usdt_market = "0xfbc729e93b05b4c48916c1433c9f9c2ddb24605a73483303ea0f87a8886b52af"
+    subaccount_id = "0xbdaedec95d563fb05240d6e01821008454c24c36000000000000000000000000"
 
-    bank_balances_filter = composer.chain_stream_bank_balances_filter()
-    subaccount_deposits_filter = composer.chain_stream_subaccount_deposits_filter()
-    spot_trades_filter = composer.chain_stream_trades_filter()
-    derivative_trades_filter = composer.chain_stream_trades_filter()
-    spot_orders_filter = composer.chain_stream_orders_filter()
-    derivative_orders_filter = composer.chain_stream_orders_filter()
-    spot_orderbooks_filter = composer.chain_stream_orderbooks_filter()
-    derivative_orderbooks_filter = composer.chain_stream_orderbooks_filter()
-    positions_filter = composer.chain_stream_positions_filter()
-    oracle_price_filter = composer.chain_stream_oracle_price_filter()
+    inj_usdt_market = "0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe"
+    inj_usdt_perp_market = "0x17ef48032cb24375ba7c2e39f384e56433bcab20cbee9a7357e4cba2eb00abe6"
+
+    bank_balances_filter = composer.chain_stream_bank_balances_filter(
+        accounts=["inj1hkhdaj2a2clmq5jq6mspsggqs32vynpk228q3r"]
+    )
+    subaccount_deposits_filter = composer.chain_stream_subaccount_deposits_filter(
+        subaccount_ids=[subaccount_id]
+    )
+    spot_trades_filter = composer.chain_stream_trades_filter(
+        subaccount_ids=["*"],
+        market_ids=[inj_usdt_market]
+    )
+    derivative_trades_filter = composer.chain_stream_trades_filter(
+        subaccount_ids=["*"],
+        market_ids=[inj_usdt_perp_market]
+    )
+    spot_orders_filter = composer.chain_stream_orders_filter(
+        subaccount_ids=[subaccount_id],
+        market_ids=[inj_usdt_market]
+    )
+    derivative_orders_filter = composer.chain_stream_orders_filter(
+        subaccount_ids=[subaccount_id],
+        market_ids=[inj_usdt_perp_market]
+    )
+    spot_orderbooks_filter = composer.chain_stream_orderbooks_filter(
+        market_ids=[inj_usdt_market]
+    )
+    derivative_orderbooks_filter = composer.chain_stream_orderbooks_filter(
+        market_ids=[inj_usdt_perp_market]
+    )
+    positions_filter = composer.chain_stream_positions_filter(
+        subaccount_ids=[subaccount_id],
+        market_ids=[inj_usdt_perp_market]
+    )
+    oracle_price_filter = composer.chain_stream_oracle_price_filter(
+        symbols=["INJ", "USDT"]
+    )
     stream = await client.chain_stream(
         bank_balances_filter=bank_balances_filter,
         subaccount_deposits_filter=subaccount_deposits_filter,
