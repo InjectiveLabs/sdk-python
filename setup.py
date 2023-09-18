ceaf0264903bc821c23f7bbc3073a0b6236430c0
abel <abarmoa@gmail.com>
2023-09-18 18:14:12 -0300

    (feat) Added info to mainnet .ini file for NEOK/USDT and ORAI/USDT markets

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = "https://github.com/InjectiveLabs/sdk-python"
 EMAIL = "achilleas@injectivelabs.com"
 AUTHOR = "Injective Labs"
 REQUIRES_PYTHON = ">=3.9"
-VERSION = "0.8.4"
+VERSION = "0.8.5"
 
 REQUIRED = [
     "aiohttp",
