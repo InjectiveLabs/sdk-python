3590e04c0538fa47817b163bd83ea797c8076de0
abel <abarmoa@gmail.com>
2024-02-06 16:50:12 -0300

    (feat) Refactored all examples to remove references to private keys. Added the use of `dotenv` to load private keys into environment variables. Removed `asyncio` package dependency

--- examples/chain_client/27_Grants.py
+++ examples/chain_client/27_Grants.py
@@ -1,14 +1,19 @@
 import asyncio
+import os
+
+import dotenv
 
 from pyinjective.async_client import AsyncClient
 from pyinjective.core.network import Network
 
 
 async def main() -> None:
+    dotenv.load_dotenv()
+    granter = os.getenv("INJECTIVE_GRANTER_PUBLIC_ADDRESS")
+    grantee = os.getenv("INJECTIVE_GRANTEE_PUBLIC_ADDRESS")
+
     network = Network.testnet()
     client = AsyncClient(network)
-    granter = "inj14au322k9munkmx5wrchz9q30juf5wjgz2cfqku"
-    grantee = "inj1hkhdaj2a2clmq5jq6mspsggqs32vynpk228q3r"
     msg_type_url = "/injective.exchange.v1beta1.MsgCreateDerivativeLimitOrder"
     authorizations = await client.fetch_grants(granter=granter, grantee=grantee, msg_type_url=msg_type_url)
     print(authorizations)
