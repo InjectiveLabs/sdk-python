d5666388af96f870b189ed030524814420bb5632
abel <abarmoa@gmail.com>
2024-02-20 12:52:10 -0300

    (feat) Added support for all chain exchange module queries. Included new example scripts for all the queries and unit tests.

---
+++ examples/chain_client/exchange/query/13_FullSpotMarket.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from pyinjective.async_client import AsyncClient
+from pyinjective.core.network import Network
+
+
+async def main() -> None:
+    # select network: local, testnet, mainnet
+    network = Network.testnet()
+
+    # initialize grpc client
+    client = AsyncClient(network)
+
+    spot_market = await client.fetch_chain_full_spot_market(
+        market_id="0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe",
+        with_mid_price_and_tob=True,
+    )
+    print(spot_market)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ examples/chain_client/exchange/query/19_TraderSpotTransientOrders.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+
+import dotenv
+
+from pyinjective import PrivateKey
+from pyinjective.async_client import AsyncClient
+from pyinjective.core.network import Network
+
+
+async def main() -> None:
+    dotenv.load_dotenv()
+    configured_private_key = os.getenv("INJECTIVE_PRIVATE_KEY")
+
+    # select network: local, testnet, mainnet
+    network = Network.testnet()
+
+    # initialize grpc client
+    client = AsyncClient(network)
+
+    # load account
+    priv_key = PrivateKey.from_hex(configured_private_key)
+    pub_key = priv_key.to_public_key()
+    address = pub_key.to_address()
+    await client.fetch_account(address.to_acc_bech32())
+
+    subaccount_id = address.get_subaccount_id(index=0)
+
+    orders = await client.fetch_chain_trader_spot_transient_orders(
+        market_id="0x0611780ba69656949525013d947713300f56c37b6175e02f26bffa495c3208fe",
+        subaccount_id=subaccount_id,
+    )
+    print(orders)
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
